0e50855e7156a25b6efa29c896166413d45cedd7
j-camposar <31163773+j-camposar@users.noreply.github.com>
2025-11-05 00:23:26 -0300

    commit de trazbilidad

---
+++ tracing.js
@@ -0,0 +1,19 @@
+// tracing.js
+'use strict';
+
+const { NodeSDK } = require('@opentelemetry/sdk-node');
+const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
+const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
+
+const traceExporter = new OTLPTraceExporter({
+  url: 'http://tempo:4318/v1/traces', // apunta al servicio Tempo dentro de la red Docker
+});
+
+const sdk = new NodeSDK({
+  traceExporter,
+  instrumentations: [getNodeAutoInstrumentations()],
+});
+
+sdk.start()
+  .then(() => console.log('✅ OpenTelemetry iniciado y exportando trazas a Tempo'))
+  .catch(err => console.error('❌ Error al iniciar OpenTelemetry', err));
